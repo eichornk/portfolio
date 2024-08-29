f3bb7435a6ae6b0534ec2ccbb9733ea29409261b
eichornk <91559450+eichornk@users.noreply.github.com>
2024-08-29 08:52:20 -0700

    Certifications and About Me

--- src/App.jsx
+++ src/App.jsx
@@ -3,54 +3,106 @@ import './App.css'
 export default function App() {
   return (
     <>
-      <div class="container border mt-5">
-        <div class="row justify-content-center">
-            <div class="border col-md-3">                                   {/* Will want to fix the spacing here eventually*/}
+      <div className="container border mt-5">
+        <div className="row justify-content-center">
+            <div className="border col-md-3 text-center">                                   {/* Will want to fix the spacing here eventually*/}
                 <h1>Hello! My name is Kaylee Eichorn</h1>
             </div>
-            <div class="border col-md-3 text-end">                           {/* And here too */}
+            <div className="border col-md-3 text-end">                           {/* And here too */}
                 <p> Image here </p>
             </div>
         </div>
-    </div>
+      </div>
 
-      <div class="container border text-center">
+    {/* About me section */}
+      <div className="container border text-center mt-5">
         <h3> About me </h3>
-        <p> Stuff about me here </p>
+        <p> Welcome to my Portfolio! My name is Kaylee and I'm currently looking for jobs in Web Development and UX Research. 
+          I graduated from Oregon State University in June 2024 with a Bachelor of Science in Computer Science focusing on Human-Computer 
+          Interactions. I chose this focus because I’m fascinated by how people perceive various applications and am eager to explore ways 
+          to make them more accessible and user-friendly for everyone. This led to me adding minors in User Experience Research and Psychology.  </p>
       </div>
 
-      <div class="container border text-center">
-        <h3> Projects </h3>
+    {/* Project Section */}
+      <div className="container border mt-5">
+        <h3 className="text-center"> Projects </h3>
 
-        <div class="card">
-          <p> Project 1 </p>
+        <div className="card mt-2">
+          <div className="row no-gutters">
+            {/* Project image here */}
+            <div className="col-md-4">
+              <img src="..." className="card-img" alt="..."/>
+            </div>
+            {/* Project details here */}
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title"> COE Major Exploration </h5>
+                <p className="card-text">Project Description</p>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div class="card">
-          <p> Project 1 </p>
+        <div className="card mt-2">
+          <div className="row no-gutters">
+            {/* Project image here */}
+            <div className="col-md-4">
+              <img src="..." className="card-img" alt="..."/>
+            </div>
+            {/* Project details here */}
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title"> Go Greek Database </h5>
+                <p className="card-text">Project Description</p>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div class="card">
-          <p> Project 1 </p>
+        <div className="card mt-2">
+          <div className="row no-gutters">
+            {/* Project image here */}
+            <div className="col-md-4">
+              <img src="..." className="card-img" alt="..."/>
+            </div>
+            {/* Project details here */}
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title"> Redesign </h5>
+                <p className="card-text">Project Description</p>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div class="card">
-          <p> Project 1 </p>
+        <div className="card mt-2">
+          <div className="row no-gutters">
+            {/* Project image here */}
+            <div className="col-md-4">
+              <img src="..." className="card-img" alt="..."/>
+            </div>
+            {/* Project details here */}
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title"> Other Project </h5>
+                <p className="card-text">Project Description</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div class="container border text-center">
+    {/* Certification Section */}
+      <div className="container border text-center mt-5">
         <h3> Certifications </h3>
-        <ul>
-          <li> Certification 1 </li>
-          <li> Certification 2 </li>
-          <li> Certification 3 </li>
-          <li> Certification 4 </li>
-          <li> Certification 5 </li>
-        </ul>
+        <p> MTA: HTML5 Application Development Fundamentals</p>
+        <p> MTA: Windows Operating System Fundamentals </p>
+        <p> MTA: Introduction to Programming Using HTML and CSS </p>
+        <p> CompTIA IT Fundamentals+ </p>
       </div>
 
-      <div class="container border text-center">
+    {/* Contact Section */}
+      <div className="container border text-center mt-5">
         <h3> Contact </h3>
         <button>Resume</button>
         <button>Github</button>

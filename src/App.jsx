d6a015cc87eaf05d0acf541f2c96e02f4f95e741
eichornk <91559450+eichornk@users.noreply.github.com>
2024-08-30 16:05:40 -0700

    Small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
 import './App.css'
+import styled from '@emotion/styled'
+import majorExp from './assets/explorationProject.png'
+import greekDB from './assets/greekDatabase.png'
+const Button = styled.button`
+  background-color: white;
+  &:hover {
+        background-color: green;
+    }
+`
+
+
 
 export default function App() {
   return (
@@ -20,7 +31,8 @@ export default function App() {
         <p> Welcome to my Portfolio! My name is Kaylee and I'm currently looking for jobs in Web Development and UX Research. 
           I graduated from Oregon State University in June 2024 with a Bachelor of Science in Computer Science focusing on Human-Computer 
           Interactions. I chose this focus because I’m fascinated by how people perceive various applications and am eager to explore ways 
-          to make them more accessible and user-friendly for everyone. This led to me adding minors in User Experience Research and Psychology.  </p>
+          to make them more accessible and user-friendly for everyone. This led to me adding minors in User Experience Research and Psychology.
+        </p>
       </div>
 
     {/* Project Section */}
@@ -31,13 +43,16 @@ export default function App() {
           <div className="row no-gutters">
             {/* Project image here */}
             <div className="col-md-4">
-              <img src="..." className="card-img" alt="..."/>
+              <img src={majorExp} className="card-img" alt="Front Page of Website"/>
             </div>
             {/* Project details here */}
             <div className="col-md-8">
               <div className="card-body">
                 <h5 className="card-title"> COE Major Exploration </h5>
-                <p className="card-text">Project Description</p>
+                <p className="card-text">My Senior Capstone project was based on helping incoming students to the College of Engineering find a major that aligned with their 
+                  interests and career goals. We created a website that allowed students to explore the different majors offered by the College of Engineering, 
+                  learn about the different career paths available to them, and find resources to help them succeed in their chosen major.
+                </p>
               </div>
             </div>
           </div>
@@ -47,13 +62,16 @@ export default function App() {
           <div className="row no-gutters">
             {/* Project image here */}
             <div className="col-md-4">
-              <img src="..." className="card-img" alt="..."/>
+              <img src={greekDB} className="card-img" alt="Go Greek Database Front Page"/> {/* Can probably make this website look better */}
             </div>
             {/* Project details here */}
             <div className="col-md-8">
               <div className="card-body">
                 <h5 className="card-title"> Go Greek Database </h5>
-                <p className="card-text">Project Description</p>
+                <p className="card-text">A Database my partner and I worked on for our Final Project in Introduction to Databases. This database is intended to be used by the 
+                  Greek Life Office at Oregon State University to help them keep track of the different Greek Organizations on campus, the members of each organization, and philanthropy
+                  events that each organization is hosting. We wanted to create this database to make it easier to access all this information in one place.
+                </p>
               </div>
             </div>
           </div>
@@ -95,7 +113,7 @@ export default function App() {
     {/* Certification Section */}
       <div className="container border text-center mt-5">
         <h3> Certifications </h3>
-        <p> MTA: HTML5 Application Development Fundamentals</p>
+        <p> MTA: HTML5 Application Development Fundamentals </p>
         <p> MTA: Windows Operating System Fundamentals </p>
         <p> MTA: Introduction to Programming Using HTML and CSS </p>
         <p> CompTIA IT Fundamentals+ </p>
@@ -104,10 +122,10 @@ export default function App() {
     {/* Contact Section */}
       <div className="container border text-center mt-5">
         <h3> Contact </h3>
-        <a href="https://docs.google.com/document/d/1zDluULlcMdWv3Z8oIeb1qhblGCDVGuVqkeEU8Wq5jcM/edit?usp=sharing">Resume</a>
-        <a href="https://github.com/eichornk">Github</a>
-        <a href="https://www.linkedin.com/in/kaylee-eichorn">LinkedIn</a>
-        <button>Other</button>
+        <Button><a href="https://docs.google.com/document/d/1zDluULlcMdWv3Z8oIeb1qhblGCDVGuVqkeEU8Wq5jcM/edit?usp=sharing" ClassName="text-decoration-none">Resume</a></Button>
+        <Button><a href="https://github.com/eichornk" ClassName="text-decoration-none">Github</a></Button>
+        <Button><a href="https://www.linkedin.com/in/kaylee-eichorn" ClassName="text-decoration-none">LinkedIn</a></Button>
+        <Button>Other</Button>
       </div>
     </>
   );
